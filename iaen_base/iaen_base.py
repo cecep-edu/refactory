d9cbc0d3b03bb32d74fea9fe37f11bbfe819a11f
Jonathan Herrera <aj@Aj-Desarrollo.(none)>
2014-07-03 16:03:09 -0500

    merger auto

--- iaen_base/iaen_base.py
+++ iaen_base/iaen_base.py
@@ -150,3 +150,21 @@ class estado_civil(osv.osv):
     _columns = {
         'name' : fields.char("Nombre", size=50, required=True),
     }
+
+
+#CLASE FAMILIAR
+class gender(osv.osv):    
+    _name="family_burden"
+    _description="Carga familiar"
+    _order = "name"        
+    _columns={
+            "name" : fields.char("Nombre",size=10,required=True),
+            "description" : fields.text("Descripcion"),
+    }
+    _order = "name"
+    _sql_constraints = [('name_uniq', 'unique(name)', _(u'Ya existe un genero con el mismo nombre'))]
+    def _no_numbers(self, cr, uid, ids):
+        for bloody_type in self.browse(cr, uid, ids):
+            if not (re.search("[a-z, A-Z]", bloody_type.name)): return False
+        return True 
+    _constraints = [(_no_numbers, _(u"El Tipo de dato es invalido."), ['name'])]
