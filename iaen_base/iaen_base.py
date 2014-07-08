336e38c6af83d257576ace990d97a3c8fd2ac667
jonathan12101986 <jonathan12101986@gmail.com>
2014-07-08 14:50:15 -0500

    actualiza DATA relacion familiar

--- iaen_base/iaen_base.py
+++ iaen_base/iaen_base.py
@@ -56,15 +56,16 @@ class gender(osv.osv):
     _description="Tipos de identidad de genero"
     _order = "name"        
     _columns={
-            "name" : fields.char("Nombre",size=10,required=True),
+            "name" : fields.char("Nombre",size=15,required=True),
             "description" : fields.text("Detalle"),
     }
     _order = "name"
     _sql_constraints = [('name_uniq', 'unique(name)', _(u'Ya existe un genero con el mismo nombre'))]
     def _alphabetical(self, cr, uid, ids):
         for bloody_type in self.browse(cr, uid, ids):
-            if not (re.search("[a-z, A-Z]", bloody_type.name)): return False
+            if  re.search("[^a-z, A-Z]", bloody_type.name): return False
         return True 
+
     _constraints = [(_alphabetical, _(u"El Tipo de dato es invalido."), ['name'])]
    
             
@@ -159,37 +160,37 @@ class family_relationship(osv.osv):
     _order = "name"
     _sql_constraints = [('name_unique', 'unique(name)', _(u'Ya existe un parentesco con el mismo nombre'))]
     _columns={
-            "name": fields.char("Nombre", size=12, required=True),
+            "name": fields.char("Nombre", size=20, required=True),
             "description": fields.text("Descripcion"),
     }
     def _alphabetical(self, cr, uid, ids):
         for bloody_type in self.browse(cr, uid, ids):
-            if not (re.search("[a-z, A-Z]", bloody_type.name)): return False
+            if  re.search("[^a-z, A-Z]", bloody_type.name): return False
         return True 
 
-    _constraints = [(_alphabetical, _(u"El Tipo de dato es invalido."), ['name'])]
+#    _constraints = [(_alphabetical, _(u"El Tipo de dato es invalido."), ['name'])]
 
 #CARGA FAMILIAR
 class family_burden(osv.osv):    
     _name = "family.burden"
     _description = "Carga Familiar"       
-    _order = "lastName"
+    _order = "last_name"
     #_sql_constraints = [('name_unique', 'unique(name)', _(u'Ya existe un parentesco con el mismo nombre'))]
     _columns={
-            "name": fields.char("Nombre", size=12, required=True),
-	    "lastName": fields.char("Apellido", size=12, required=True),
-            "typeId": fields.many2one("identification.type", "Tipo de identificacion"),
-            "numberId": fields.char("Nro Identificacion", size=15, required=True),
-            "typeRelFamily": fields.many2one("family.relationship","Tipo de Relacion"),
-            "dateBirth": fields.date("Fecha nacimiento", required=True),
+            "name": fields.char("Nombre", size=20, required=True),
+	    "last_name": fields.char("Apellido", size=20, required=True),
+            "type_id": fields.many2one("identification.type", "Tipo de identificacion"),
+            "number_id": fields.char("Nro Identificacion", size=15, required=True),
+            "type_rel_family": fields.many2one("family.relationship","Tipo de Relacion"),
+            "date_birth": fields.date("Fecha nacimiento", required=True),
             "phone": fields.char("Telefono", size=10, requiered=True),
             "movil": fields.char("Celular", size=10, requiered=True),            
-            "checkContactSos": fields.boolean("Contacto emergencia", requiered=True),
+            "type_instruction": fields.many2one("instruction","Instruccion"),
+            "check_contact_sos": fields.boolean("Contacto emergencia", requiered=True),
     }
     _defaults = {
-        "checkContactSos": False,
-        }
-   
+        "check_contact_sos": False,
+        }    
 #"instruction": fields.many2one("instruction", "Instruccion"),
 
 #    _constraints = [(_no_numbers, _(u"El Tipo de dato es invalido."), ['name'])]
