8fb65dd8eaae8454f161953c6d7c8953214d5aee
Mauricio Fierro <mauricio@maquiavelica-x2>
2014-07-11 15:45:19 -0500

    Tamaño y orden de los campos ajustado. Quitadas las agrupaciones innecesarias. Validado solo ocurre cuando el Tipo de Identificación es Cédula.

--- iaen_curriculum/iaen_curriculum.py
+++ iaen_curriculum/iaen_curriculum.py
@@ -52,7 +52,7 @@ class curriculum(osv.osv):
 		"disability": fields.boolean("Discapacidad"),
 		"disability_id": fields.many2one("type.disability", "Tipo de Discapacidad"), 
 		"disability_degree": fields.char("Grado de Discapacidad", size=150), 
-		"conadis_number": fields.char("Número del Carnet del CONADIS.", size=10, require=True),
+		"conadis_number": fields.char("N° Carnet del CONADIS", size=10, require=True),
 		"ethnic_group_id": fields.many2one("ethnic.group", u"Grupo Étnico", required=True),
 		"family_burden_ids": fields.one2many("family.burden", "curriculum_id", 'Carga Familiar', required=False),
 		"bank_info_ids": fields.one2many("bank.info", "curriculum_id", "Información Bancaria", required=False),
@@ -64,7 +64,8 @@ class curriculum(osv.osv):
 	}
 	def _only_numbers(self, cr, uid, ids):
 		for curriculum in self.browse(cr, uid, ids):
-			if not (re.search("^-?[0-9]+$", curriculum.identification_number)): return False
+			if curriculum.identification_type_id.name.lower().find(u"cédula")>=0 and not (re.search("^-?[0-9]+$", curriculum.identification_number)): 
+				return False
 		return True 
 	def on_disability(self, cr, uid, ids, disability):
 		if not disability:
